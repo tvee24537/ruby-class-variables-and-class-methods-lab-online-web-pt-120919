1fc93b8d8f69c58e6e403593e0323ddaea2ad559
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-12 21:33:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,13 @@ class Song
     @@songCount
   end
   
-  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
   
 end
