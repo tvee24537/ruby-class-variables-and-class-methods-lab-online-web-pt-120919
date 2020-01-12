7fcc353cb279f79ea70bfc17c4135222fa25b53a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-12 21:36:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,5 +18,16 @@ class Song
     @@songCount +=1
   end
   
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   
 end
